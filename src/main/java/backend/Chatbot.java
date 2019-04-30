c5b8921efe0753fd7c361b53178c9ea1abaff72e
HamzaAli1 <hamzaimran2011@gmail.com>
2019-04-30 10:56:55 -0500

    Initial commit

---
+++ src/main/java/backend/Chatbot.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backend;
+
+/**
+ *
+ * @author Hamza Ali
+ */
+public class Chatbot {
+    
+    /** access API like this:
+     * http://api.intellexer.com/[GET/POST method]?apikey={YourAPIKey}&options={options}
+     */
+    private static final String WEBSITE = "http://api.intellexer.com/";
+    private static final String API_KEY = "81e41a7b-e1a0-469e-89da-8002eb5a3a96";
+
+    public Chatbot() {
+    }
+    
+    public String respond(String input) {
+        
+        return "";
+    }
+}
---
+++ src/main/java/frontend/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package frontend;
+
+/**
+ *
+ * @author Hamza Ali
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO main runner file
+        new MainWindow().setVisible(true);
+    }
+    
+}
